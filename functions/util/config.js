a3adc06f5043b842dcc624bc59f2143733d8b7b4
damephena <okoyeifenna24@gmail.com>
2020-03-03 22:13:54 +0100

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyA6uUkJn4s1kwlqZhKtLsJiabDMOqffNi8",
+    authDomain: "chatty-a17f8.firebaseapp.com",
+    databaseURL: "https://chatty-a17f8.firebaseio.com",
+    projectId: "chatty-a17f8",
+    storageBucket: "chatty-a17f8.appspot.com",
+    messagingSenderId: "995246813403",
+    appId: "1:995246813403:web:d9b5f56e39013342325cfe",
+    measurementId: "G-HDQKYY9L7C"
+}
